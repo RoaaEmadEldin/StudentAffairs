130d39cdf5b32d2a6eaf0c8885feaf8b3e6ebc1e
mariamayman10 <101516694+mariamayman10@users.noreply.github.com>
2023-04-11 11:26:56 +0200

    Add files via upload

---
+++ Student.js
@@ -0,0 +1,18 @@
+class Student{
+    constructor(FirstName, MiddleName, LastName, ID, DateOfBirth, Status, GPA, Gender, Level, Department, Email, PhoneNumber){
+        this.FirstName = FirstName;
+        this.MiddleName = MiddleName;
+        this.LastName = LastName;
+        this.ID = ID;
+        this.DateOfBirth = DateOfBirth;
+        this.Status = Status;
+        this.GPA = GPA;
+        this.Gender = Gender;
+        this.Level = Level;
+        this.Department = Department;
+        this.Email = Email;
+        this.PhoneNumber = PhoneNumber;
+    }
+};
+
+
