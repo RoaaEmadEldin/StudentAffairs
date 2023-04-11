6ed3a76bed58b1b56f22b3e483e86c64e8901ea1
mariamayman10 <101516694+mariamayman10@users.noreply.github.com>
2023-04-11 11:26:10 +0200

    Local Storage Added

--- AddStudent.js
+++ AddStudent.js
@@ -1,61 +1,62 @@
 function validate() {
     let trueinfo = true;
-
     let CheckEmail = document.getElementById("email").value;
     const regex = /^[0-9]+@stud.uni.edu.eg$/;
     if (!regex.test(CheckEmail) && CheckEmail != "") {
         trueinfo = false;
-        event.preventDefault()
         alert("Invalid Email, Please Enter Valid Email");
     }
-    let passsword = document.getElementById("Pass").value;
-    let confirmPassword = document.getElementById("ppass").value;
-    const strongPasswordRegex = /^.*(?=.{8,})(?=.*[a-zA-Z])(?=.*\d)(?=.*[!#$%&? "]).*$/
-    ;
-    if (!strongPasswordRegex.test(passsword)) {
-        trueinfo = false;
-        event.preventDefault()
-        alert("Password must contain at least one lowercase letter, one uppercase letter, one digit, one special character, and be at least 8 characters long");
-    }
-    if (passsword !== confirmPassword) {
-        trueinfo = false;
-        event.preventDefault()
-        alert("Passwords do not match");}
 
-
-    let Checkid = document.getElementById("ID").value;
-
+    let Checkid = document.getElementById("id").value;
     if (Checkid.length != 8) {
         trueinfo = false;
-        event.preventDefault()
         alert("Invalid ID, Please Enter Valid ID");
     }
 
     let Checkgpa = document.getElementById("gpa").value;
     if ((Checkgpa < 0 || Checkgpa > 4) && Checkgpa != "") {
         trueinfo = false;
-        event.preventDefault()
         alert("Invalid GPA, Please Enter Valid GPA");
     }
+
     let Checkname = document.getElementsByClassName("Name").value;
     const regex2 = /[^A-Za-z]/g;
     if (!regex2.test(Checkn) && Checkn != "") {
         trueinfo = false;
         alert("Invalid Name, Please Enter Valid Name");
     }
-    let CheckPhone = document.getElementById("number").value;
+
+    let CheckPhone = document.getElementById("phone").value;
     if ((CheckPhone.length != 11 || CheckE[0] != 0 || CheckE[1] != 1) && CheckPhone != "") {
         trueinfo = false;
         alert("Invalid Mobile Number, Please Enter Valid Mobile Number");
     }
-    let Checkdate = document.getElementById("date").value;
+
+    let Checkdate = document.getElementById("dob").value;
     let inputdate = new Date(Checkdate);
     let currentdate = new Date()
     if (inputdate > currentdate && Checkdate != "") {
         trueinfo = false;
         alert("Invalid Date, Please Enter Valid Date");
     }
-
-
+    return trueinfo;
 }
-//Rana#2114
+
+function SaveStudent(){
+    let FN = document.getElementById("firstname").value;
+    let MN = document.getElementById("middlename").value;
+    let LN = document.getElementById("lastname").value;
+    let ID = document.getElementById("id").value;
+    let L = document.getElementById("level").value;
+    let D = document.getElementById("dep").value;
+    let Gpa = document.getElementById("gpa").value;
+    let Gen = document.getElementById("gender").value;
+    let S = document.getElementById("status").value;
+    let Ph = document.getElementById("phone").value;
+    let Dob = document.getElementById("dob").value;
+    let Em = document.getElementById("email").value;
+    let newstudent = new Student(FN, MN, LN, ID, Dob, S, Gpa, Gen, L, D, Em, Ph);
+    Students.push(newstudent);
+    localStorage.setItem('Students', JSON.stringify(Students));
+}
+   
