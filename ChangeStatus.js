b3a0d06373d10a3f781bf1db4ce226776eb9e200
Habiba Ihab <101571848+habibaihab@users.noreply.github.com>
2023-04-12 20:04:25 +0200

    Update ChangeStatus.js

--- ChangeStatus.js
+++ ChangeStatus.js
@@ -1,12 +1,93 @@
-function changeStatus(id) {
-    var status = document.querySelector("tr:nth-child(" + id + ") td:nth-child(10)");
-    if (status.classList.contains("active")) {
-      status.classList.remove("active");
-      status.classList.add("inactive");
-      status.innerHTML = "Inactive";
-    } else {
-      status.classList.remove("inactive");
-      status.classList.add("active");
-      status.innerHTML = "Active";
-    }
+function updateStudentStatus(id, status) {
+  var studentsData = localStorage.getItem("Students");
+  var students = JSON.parse(studentsData);
+
+  if (Array.isArray(students) && students.length > 0) {
+      for (var i = 0; i < students.length; i++) {
+          if (students[i].ID === id) {
+              students[i].Status = status;
+              localStorage.setItem("Students", JSON.stringify(students));
+              break;
+          }
+      }
+  } else {
+      console.error("Invalid or empty data in localStorage: ", students);
   }
+}
+function createStudentRow(student) {
+  var row = document.createElement("tr");
+
+  var nameCell = document.createElement("td");
+  nameCell.innerHTML = student.Name;
+  row.appendChild(nameCell);
+
+  var idCell = document.createElement("td");
+  idCell.innerHTML = student.ID;
+  row.appendChild(idCell);
+
+  var dobCell = document.createElement("td");
+  dobCell.innerHTML = student.DateOfBirth;
+  row.appendChild(dobCell);
+
+  var statusCell = document.createElement("td");
+  statusCell.innerHTML = student.Status;
+  row.appendChild(statusCell);
+
+  var gpaCell = document.createElement("td");
+  gpaCell.innerHTML = student.GPA;
+  row.appendChild(gpaCell);
+
+  var deptCell = document.createElement("td");
+  deptCell.innerHTML = student.Department;
+  row.appendChild(deptCell);
+  var emailCell = document.createElement("td");
+  emailCell.innerHTML = student.Email;
+  row.appendChild(emailCell);
+
+ var genderCell = document.createElement("td");
+ genderCell.innerHTML = student.Gender;
+ row.appendChild(genderCell);
+
+ var levelCell = document.createElement("td");
+ levelCell.innerHTML = student.Level;
+ row.appendChild(levelCell);
+
+ var phoneCell = document.createElement("td");
+ phoneCell.innerHTML = student.PhoneNumber;
+ row.appendChild(phoneCell);
+ var actionCell = document.createElement("td");
+
+ var statusButton = document.createElement("button");
+ statusButton.innerHTML = "Change Status";
+ statusButton.addEventListener("click", function() {
+ var newStatus;
+ if (student.Status === "active") {
+   newStatus = "inactive";
+  } else {
+  newStatus = "active";
+ }
+ updateStudentStatus(student.ID, newStatus);
+ student.Status = newStatus;
+ statusCell.innerHTML = newStatus;
+ });
+ actionCell.appendChild(statusButton);
+ row.appendChild(actionCell);
+ return row;
+ }
+
+ function renderStudentsTable() {
+ var studentsData = localStorage.getItem("Students");
+ var students = JSON.parse(studentsData);
+ if (Array.isArray(students) && students.length > 0) {
+  var studentsTableBody = document.getElementById("studentsTableBody");
+
+  for (var i = 0; i < students.length; i++) {
+      var studentRow = createStudentRow(students[i]);
+      studentsTableBody.appendChild(studentRow);
+  }
+ } else {
+  console.error("Invalid or empty data in localStorage: ", students);
+ }
+ }
+ renderStudentsTable();
+
