5a77deca608757fe4b2fc7074e172d8fb625cf3d
mariamayman10 <101516694+mariamayman10@users.noreply.github.com>
2023-04-13 16:44:55 +0200

    Update Update&Delete.js

--- Update&Delete.js
+++ Update&Delete.js
@@ -1,40 +1,39 @@
 function CheckInfo1(){
-    let trueinfo = true;
     let Checkid = document.getElementById("ID").value;
     if(Checkid.length != 8 ){
-        trueinfo = false;
         alert("Invalid ID, Please Enter Valid ID");
-    }
-    let CheckE = document.getElementById("email").value;
-    const regex1 = /^[0-9]+@stud.uni.edu.eg$/;
-    if(!regex1.test(CheckE) && CheckE != ""){
-        trueinfo = false;
-        alert("Invalid Email, Please Enter Valid Email");
-    }
-    let Checkgpa = document.getElementById("gpa").value;
-    if((Checkgpa < 0 || Checkgpa > 4) && Checkgpa != ""){
-        trueinfo = false;
-        alert("Invalid GPA, Please Enter Valid GPA");
+        return false;
     }
     let Checkn = document.getElementById("Name").value;
     const regex2 = /^[a-zA-Z.]+\s[a-zA-Z.]+\s[a-zA-Z.]+$/;
     if(!regex2.test(Checkn) && Checkn != ""){
-        trueinfo = false;
         alert("Invalid Name, Please Enter Valid Name");
+        return false;
+    }
+    let Checkgpa = document.getElementById("gpa").value;
+    if((Checkgpa < 0 || Checkgpa > 4) && Checkgpa != ""){
+        alert("Invalid GPA, Please Enter Valid GPA");
+        return false;
+    }
+    let CheckE = document.getElementById("email").value;
+    const regex1 = /^[0-9]+@stud.uni.edu.eg$/;
+    if(!regex1.test(CheckE) && CheckE != ""){
+        alert("Invalid Email, Please Enter Valid Email");
+        return false;
     }
     let CheckPhone = document.getElementById("phone").value;
     if((CheckPhone.length != 11 || CheckE[0] != 0 || CheckE[1] != 1) && CheckPhone != ""){
-        trueinfo = false;
         alert("Invalid Mobile Number, Please Enter Valid Mobile Number");
+        return false;
     }
     let Checkdate = document.getElementById("date").value;
     let inputdate = new Date(Checkdate);
     let currentdate = new Date()
     if(inputdate > currentdate && Checkdate != ""){
-        trueinfo = false;
         alert("Invalid Date, Please Enter Valid Date");
+        return false;
     }
-    return trueinfo;
+    return true;
 }
 function CheckInfo2(){
     let trueinfo = true;
@@ -46,6 +45,7 @@ function CheckInfo2(){
     return trueinfo;
 }
 function UpdateStudent(){
+    let updForm = document.getElementById("update");
     let Students = JSON.parse(localStorage.getItem("Students"));
     let Studentid = document.getElementById("ID").value;
     let Studentgpa = document.getElementById("gpa").value;
@@ -56,8 +56,11 @@ function UpdateStudent(){
     let StudentDob = document.getElementById("date").value;
     let StudentStatus = document.getElementById("status").value;
     let StudentGender = document.getElementById("gender").value;
-    let Studentfound = false;
-    for(let i = 0;i < Students.length;i++){
+    updForm.addEventListener("submit", function(event){
+        let updinfo = CheckInfo1();
+        if(updinfo){
+            let Studentfound = false;
+            for(let i = 0;i < Students.length;i++){
         if(Studentid == Students[i].ID){
             Studentfound = true;
             if(Studentgpa != ""){
@@ -85,46 +88,40 @@ function UpdateStudent(){
                 Students[i].Gender = StudentGender;
             }
         }
-    }
-    if(Studentfound){
-        localStorage.setItem("Students", JSON.stringify(Students));
-    }
-    else{
-        alert("There is no Student with this Id to be Updated")
-    }
+            }
+            if(Studentfound){
+                localStorage.setItem("Students", JSON.stringify(Students));
+            }
+            else{
+                alert("There is no Student with this Id to be Updated")
+            }
+        }
+    });
+    
 }
 function DeleteStudent(){
+    let delForm = document.getElementById("delete");
     let Students = JSON.parse(localStorage.getItem("Students"));
     let Studentid = document.getElementById("ID2").value; 
-    let Studentfound = false;
-    for(let i = 0;i < Students.length;i++){
-        if(Students[i].ID == Studentid){
-            Students = Students.filter((element, index) => index != i);
-            Studentfound = true;
-            break;
+    delForm.addEventListener("submit", function(event){
+        let delinfo = CheckInfo2();
+        if(delinfo){
+            let Studentfound = false;
+            for(let i = 0;i < Students.length;i++){
+                if(Students[i].ID == Studentid){
+                    Students = Students.filter((element, index) => index != i);
+                    Studentfound = true;
+                    break;
+                }
+            }
+            if(Studentfound){
+                localStorage.setItem("Students", JSON.stringify(Students));
+            }
+            else{
+                alert("There is no Student with this Id to be Deleted")
+            }
         }
-    }
-    if(Studentfound){
-        localStorage.setItem("Students", JSON.stringify(Students));
-    }
-    else{
-        alert("There is no Student with this Id to be Deleted")
-    }
+    });
 }
-function Process1(){
-    let success = false;
-    if(CheckInfo1()){
-        UpdateStudent()
-        success = true;
-    }
-    return success;
-}
-function Process2(){
-    let success = false;
-    if(CheckInfo2()){
-        DeleteStudent()
-        success = true;
-    }
-    return success;
-}
+
 
