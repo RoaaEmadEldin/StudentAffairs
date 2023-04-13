96583fe859bebf9c0ab50c904ffe26ce2ca70a0a
mariamayman10 <101516694+mariamayman10@users.noreply.github.com>
2023-04-13 16:43:39 +0200

    Update AddStudent.js

--- AddStudent.js
+++ AddStudent.js
@@ -1,50 +1,50 @@
 function validate() {
-    let trueinfo = true;
-    let CheckEmail = document.getElementById("email").value;
-    const regex = /^[0-9]+@stud.uni.edu.eg$/;
-    if (!regex.test(CheckEmail) && CheckEmail != "") {
-        trueinfo = false;
-        alert("Invalid Email, Please Enter Valid Email");
-    }
-
-    let Checkid = document.getElementById("id").value;
-    if (Checkid.length != 8) {
-        trueinfo = false;
-        alert("Invalid ID, Please Enter Valid ID");
-    }
-
-    let Checkgpa = document.getElementById("gpa").value;
-    const regex3 = /^([0-3](\.[0-9]{1,2})?|4(\.0{1,2})?)$/;
-
-    if (Checkgpa == "" || !regex3.test(Checkgpa)) {
-        trueinfo = false;
-        alert("Invalid GPA, Please Enter Valid GPA");
-    }
-
     let Checkname = document.getElementById("name").value;
     const regex2 = /^[a-zA-Z.]+\s[a-zA-Z.]+\s[a-zA-Z.]+$/;
     if (!regex2.test(Checkname) && Checkname != "") {
-        trueinfo = false;
         alert("Invalid Name, Please enter your full name.");
+        return false;
     }
-
-    let CheckPhone = document.getElementById("phone").value;
-    if ((CheckPhone.length != 11 || CheckE[0] != 0 || CheckE[1] != 1) && CheckPhone != "") {
-        trueinfo = false;
-        alert("Invalid Mobile Number, Please Enter Valid Mobile Number");
+    let Checkid = document.getElementById("id").value;
+    if (Checkid.length != 8) {
+        alert("Invalid ID, Please Enter Valid ID");
+        return false;
     }
-
+    let Checkgpa = document.getElementById("gpa").value;
+    const regex3 = /^([0-3](\.[0-9]{1,2})?|4(\.0{1,2})?)$/;
+    if (Checkgpa == "" || !regex3.test(Checkgpa)) {
+        alert("Invalid GPA, Please Enter Valid GPA");
+        return false;
+    }
+    let CheckEmail = document.getElementById("email").value;
+    const regex = /^[0-9]+@stud.uni.edu.eg$/;
+    if (!regex.test(CheckEmail) && CheckEmail != "") {
+        alert("Invalid Email, Please Enter Valid Email");
+        return false;
+    }
     let Checkdate = document.getElementById("dob").value;
     let inputdate = new Date(Checkdate);
     let currentdate = new Date()
     if (inputdate > currentdate && Checkdate != "") {
-        trueinfo = false;
         alert("Invalid Date, Please Enter Valid Date");
+        return false;
     }
-    return trueinfo;
+    
+    let CheckPhone = document.getElementById("phone").value;
+    if ((CheckPhone.length != 11 || CheckE[0] != 0 || CheckE[1] != 1) && CheckPhone != "") {
+        alert("Invalid Mobile Number, Please Enter Valid Mobile Number");
+        return false;
+    }
+    let level = document.getElementById("level").value;
+    let dep = document.getElementById("dep").value;
+    if(dep != "General" && (level == "Level 1" || level == "Level 2")){
+        alert("Invalid Department, Levels below 3 must be General");
+        return false;
+    }
+    return true;
 }
-
 function SaveStudent(){
+    let form = document.getElementById("myForm");
     let N = document.getElementById("name").value;
     let ID = document.getElementById("id").value;
     let L = document.getElementById("level").value;
@@ -55,15 +55,14 @@ function SaveStudent(){
     let Ph = document.getElementById("phone").value;
     let Dob = document.getElementById("dob").value;
     let Em = document.getElementById("email").value;
-    let newstudent = new Student(N, ID, Dob, S, Gpa, Gen, L, D, Em, Ph);
-    Students.push(newstudent);
-    localStorage.setItem('Students', JSON.stringify(Students));
+    form.addEventListener("submit", function(event){
+        let info = validate();
+        if(info){
+            let newstudent = new Student(N, ID, Dob, S, Gpa, Gen, L, D, Em, Ph);
+            Students.push(newstudent);
+            localStorage.setItem('Students', JSON.stringify(Students));
+        }
+    });
+    
 }
-function Process(){
-    let success = false;
-    if(validate()){
-        SaveStudent();
-        success = true;
-    }
-    return success;
-}
+
