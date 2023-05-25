306b65130bc3382bc8090d539d017341789eff01
Roaa Emad Eldin Muhammad <102311933+RoaaEmadEldin@users.noreply.github.com>
2023-05-25 19:12:38 +0300

    Update urls.py

--- StudentAffairsProject/Application/urls.py
+++ StudentAffairsProject/Application/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.HomePage, name = 'HomePage'),
     path('AssignDep/', views.AssignDep, name = 'AssignDep'),
-    path('AssignDep/Assign/<int:ID>/<str:Department>/', views.Assign, name = 'Assign')
+    path('Assign', views.Assign, name = 'Assign'),
 ]
